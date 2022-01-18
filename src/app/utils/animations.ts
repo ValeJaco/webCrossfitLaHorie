f37787b42bf194467dcd49241e955348b8ae4256
ValeJaco <valerian.jacobi@lgcf.fr>
2022-01-18 22:22:29 +0100

    web version

--- src/app/utils/animations.ts
+++ src/app/utils/animations.ts
@@ -4,15 +4,15 @@ export const smoothAppearing =
   trigger('smoothAppearing', [
     transition(':enter', [
       style({opacity: 0}),
-      animate('1s', style({opacity: 1}))
+      animate('.5s', style({opacity: 1}))
     ])
   ]);
 
 export const slideInOutLeftToRight =
   trigger('slideInOutLeftToRight', [
     transition(':enter', [
-      style({transform: 'translateX(-10%)', opacity: 0}),
-      animate('1s', style({transform: 'translateX(0%)', opacity: 1}))
+      style({transform: 'translateX(-140%)', opacity: 0}),
+      animate('1.3s', style({transform: 'translateX(0%)', opacity: 1}))
     ]),
     transition(':leave', [
       animate('1s', style({transform: 'translateX(40%)', opacity: 0}))
